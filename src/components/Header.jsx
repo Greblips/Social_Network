d68924944fbcb18c52f572787cd18fcb69979681
Greblips <Evgenspilbegrg@gmail.com>
2023-07-12 15:39:19 +0300

    start social network

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                className="header__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"
+                alt="day's playlist"
+            />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://images.unsplash.com/photo-1636990844278-b5e0343a1288?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1889&q=80'></img>
+            </div>
+            <div>Ava+ descripition</div>
+            <div>My posts</div>
+            <div>New post</div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    );
+};
+
+export default Profile;
